0e0294425503593a6e4577674203da75b60c917c
Leo-Huber <leohub3r@gmail.com>
2025-08-26 23:20:33 -0300

    commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { Panel } from './Panel'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Panel />
+  </StrictMode>,
+)
